e7f9a8960333acf9781e3d3bd9116bc396ae4727
Kate <ekaterina21051988@yandex.by>
2023-06-21 20:11:06 +0300

    refactor: controller file migrated to ts

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,6 +1,9 @@
 import AppLoader from './appLoader';
+import { ResponseObj } from '../../types/types';
+
+type getFunction<T> = (data?: T) => void;
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: getFunction<ResponseObj>) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -9,13 +12,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: getFunction<ResponseObj>) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -30,7 +33,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
